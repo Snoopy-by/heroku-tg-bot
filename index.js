d6cce6e73e6367fe7b609b11cda00a4dd31d8933
Snoopy-by <kirill.pats1993@gmail.com>
2020-10-16 19:32:21 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = '1345286541:AAHQeS4U2uKlhbK9vaZjvBgcrIErENIEskY'
-TelegramBot = new TelegramBot(TOKEN,{polling: true});
+const bot = new TelegramBot(TOKEN,{polling: true});
 bot.on('message', msg =>{
     bot.sendMessage(msg.chat.id, 'Hello from HEROKU!')
 })

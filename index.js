a0b6cfe7fc4b08a52a25759d58f36563dbda8e49
Snoopy-by <kirill.pats1993@gmail.com>
2020-10-16 20:52:47 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api")
-
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})
 const TOKEN = '1345286541:AAHQeS4U2uKlhbK9vaZjvBgcrIErENIEskY'
 
 const bot = new TelegramBot(TOKEN,{polling: true})
